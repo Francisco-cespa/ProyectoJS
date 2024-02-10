749975dafeedb60cf560b4fd71913521629c3128
Francisco Cespa <franciscocespa@gmail.com>
2024-02-10 16:51:20 -0300

    proyecto final

--- Proyecto Final/main.js
+++ Proyecto Final/main.js
@@ -1,5 +1,5 @@
 
-const productosBase = []
+const productosBase = [];
 
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
@@ -10,53 +10,47 @@ const traerProductos = async () => {
     try {
         const response = await fetch("./data.json");
         const productos = await response.json();
-             
+        
         productos.forEach(item => {
             productosBase.push(item)   
         })
+        
+        productos.forEach((producto) => {
+            
+            const {nombre, precio, categoria, imagen, id} = producto
+            
+            let card = document.createElement("div");
+            
+            card.className = "tarjetas"
+            card.innerHTML = `
+            <div class="card border-2 border-success" style="width: 18rem;">
+            <img src="${imagen}" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">${nombre}</h5>
+            <p> Categoria: ${categoria} </p>
+            <p class="card-text">Precio: $${precio} </p>
+            <button id="${id}" class="btn btn-secondary comprar">Agregar a carrito</button>
+            </div>
+            </div>
+            `
+            divProductos.appendChild(card)
+            
+        });
+        
     } catch (error) {
         console.error("error de acceso a base de datos", error);
     }
-
+    
 };
 
-traerProductos();
-
-console.log(productosBase);
-
-const mostrarProductos = () => {
-    
-    productosBase.forEach((producto) => {
-
-        const {nombre, precio, categoria, imagen, id} = producto
-        
-        let card = document.createElement("div");
-        
-        card.className = "tarjetas"
-        card.innerHTML = `
-        <div class="card" style="width: 18rem;">
-        <img src="${imagen}" class="card-img-top">
-        <div class="card-body">
-        <h5 class="card-title">${nombre}</h5>
-        <p> categoria: ${categoria} </p>
-        <p class="card-text">$${precio} </p>
-        <button id="${id}" class="btn btn-secondary comprar">Agregar a carrito</button>
-        </div>
-        </div>
-        `
-        divProductos.appendChild(card)
-    
-    });
-}
-
-mostrarProductos();
-
+traerProductos()
 
 const agregarAlCarrito = e => {
     if (e.target.classList.contains('comprar')) {
         const id = e.target.id;
         const producto = productosBase.find(producto => producto.id == id);
         carrito.push(producto);
+
         mostrarCarrito();
         guardarCarrito();
     }
@@ -68,7 +62,7 @@ const precioTotal = document.getElementById("precioTotal");
 
 const mostrarCarrito = () => {
     const divCarrito = document.getElementById("divCarrito");
-    divCarrito.innerHTML = "<h2> Carrito: </h2>";
+    divCarrito.innerHTML = `<h2 class="fw-semibold text-decoration-underline" > Carrito: </h2>`;
     carrito.forEach((productos) => {
         divCarrito.innerHTML += `
         <p>${productos.nombre} $${productos.precio}</p>
@@ -97,10 +91,35 @@ function limpiarCarrito() {
             
         title: "vaciado.",
         text: "su carrito esta vacio!.",
-        icon: "success"
+        icon: "warning"
     });
+}
+
+const compra = document.getElementById("finalizarCompra");
+
+const realizarCompra = () => {
+    Swal.fire({
+        title: "¿Desea terminar su compra?",
+        showDenyButton: true,
+        confirmButtonText: "Realizar compra.",
+        denyButtonText: `Continuar comprando.`
+    }).then((result) => {
+        if (result.isConfirmed) {
+            carrito = [];
+            const divCarrito = document.getElementById("divCarrito");
+            divCarrito.innerHTML = " "
+            precioTotal.innerText = "total carrito = $ " + carrito.reduce((acum, item) => acum + item.precio,
+            0,);
+            guardarCarrito();
+            Swal.fire("compra realizada.", "", "success");
+        } else if (result.isDenied) {
+          Swal.fire("Puede seguir agregando productos.", "", "info");
+        }
+      });
 }
     
 const vaciar = vaciarCarrito.addEventListener("click", limpiarCarrito)
+const comprar = compra.addEventListener("click", realizarCompra)
 
 mostrarCarrito();
+
